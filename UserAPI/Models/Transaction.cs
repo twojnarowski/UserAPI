5c9f57369ed29a79c9e38651025f76bf52934a06
tymonomyt <tymoteusz1138@gmail.com>
2021-05-11 12:44:49 +0200

    Added automatic IDs

--- UserAPI/Models/Transaction.cs
+++ UserAPI/Models/Transaction.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UserAPI.Models
 {
     public class Transaction
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
 
         public string UserId { get; set; }
--- UserAPI/Models/User.cs
+++ UserAPI/Models/User.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UserAPI.Models
 {
     public class User
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
 
         public string Login { get; set; }
